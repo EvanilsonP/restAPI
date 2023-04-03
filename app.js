8bac87232c5e7eb29d75c181da31996fe00eef4d
EvanilsonP <pevanilson99@gmail.com>
2023-04-03 11:49:32 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+var corOptions = {
+    origin: 'http://localhost:8081'
+};
+
+// middleware
+app.use(cors(corOptions));
+app.use(express.json());
+app.use(express.urlencoded({ urlencoded: true }));
+
+// testing API
+app.get('/', (req, res) => {
+    res.json({ message: 'hello from API '});
+});
+
+// PORT
+const PORT = process.env.PORT || 8080;
+
+// server
+app.listen(PORT, () => console.log('Up and running.'));
